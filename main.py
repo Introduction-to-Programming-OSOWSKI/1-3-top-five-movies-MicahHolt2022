e4b44b4be1839809d3d67e9ffc109ec168810105
MicahHolt2022 <mccormickprophet@gmail.com>
2021-09-21 08:59:23 -0500

    First Attempt

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+#define topFiveMovies
+def topFiveMovies(a,b,c,d,e):
+    #return with spaces
+    return a + " " + b + " " + c + " " + d + " " + e
+#test
+print(topFiveMovies("Megamind", "Rogue One", "La La Land", "Knives Out", "The Empire Strikes Back"))
